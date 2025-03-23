02a7dfe49fd12cb5e2c53422dc37923487853e70
Magdalena Synowiec <247796@edu.p.lodz.pl>
2025-03-23 21:18:34 +0100

    Files changes

--- ConcurrentProgramming/Data/obj/Release/net8.0/Data.AssemblyInfo.cs
+++ ConcurrentProgramming/Data/obj/Release/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b29e5028534c565df5f9ba5fc955f16253ff497")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72949f6c842d8632526f6df89652fda4bfde06de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
