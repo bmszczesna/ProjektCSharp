516c77b00701a24a588ca56aae266ed347836204
Magdalena Synowiec <247796@edu.p.lodz.pl>
2025-03-26 21:07:55 +0100

    MainWindow change

--- ConcurrentProgramming/Presentation/View/MainWindow.xaml.cs
+++ ConcurrentProgramming/Presentation/View/MainWindow.xaml.cs
@@ -1,15 +1,32 @@
 ﻿using System.Windows;
+using ViewModel;
 
 namespace Presentation.View
 {
     /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
+    /// View implementation
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            Random random = new Random();
             InitializeComponent();
+            MainWindowViewModel viewModel = (MainWindowViewModel)DataContext;
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+            viewModel.Start(random.Next(5, 10));
+        }
+
+        /// <summary>
+        /// Raises the <seealso cref="System.Windows.Window.Closed"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
+        protected override void OnClosed(EventArgs e)
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+                viewModel.Dispose();
+            base.OnClosed(e);
         }
     }
 }
